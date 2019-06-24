3393e175e0b3da09553f16184b73f911059f29b1
bradley.stinchcomb <bradley.stinchcomb@gmail.com>
2019-06-24 17:13:42 -0600

    Lsseon 19 Statck, easie than Weave

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,25 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    push(record){
+        this.data.push(record);
+    }
+
+    pop(){
+        return this.data.pop();
+    }
+
+    peek(){
+        if(this.data.length > 0){
+            return this.data[this.data.length - 1];
+          }
+          else { return null; }
+    }
+}
 
 module.exports = Stack;
